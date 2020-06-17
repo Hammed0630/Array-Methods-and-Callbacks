b8cd3a7e22913ed03777dd23f5195ab382bfc6a5
Hammed0630 <63410741+Hammed0630@users.noreply.github.com>
2020-06-17 15:53:24 -0400

    updated

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ function getFinals(/* code here */) {
 
     /* code here */
 
-};
+} ;
 
 /* Task 3: Implement a higher-order function called `getYears` that accepts the callback function `getFinals`, and returns an array called `years` containing all of the years in the dataset */
 
